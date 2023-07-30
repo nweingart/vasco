24b352ed683bb2faa8c37acbd4513390d6ed1ad8
nweingart <76226527+nweingart@users.noreply.github.com>
2023-07-30 17:55:06 -0400

    add new photo upload util

--- vasco/utils/uploadImage.js
+++ vasco/utils/uploadImage.js
@@ -1,43 +1,41 @@
-const uploadImages = async (images) => {
-  const uploadPromises = images.map(async (image, position) => { // Add 'position' parameter
-    if (!image) return null;
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { v4 as uuidv4 } from 'uuid';
 
-    const response = await fetch(image);
-    const blob = await response.blob();
-
-    const storageRef = ref(storage, `images//${position}`);
-    const uploadTask = uploadBytesResumable(storageRef, blob);
-
-    return new Promise((resolve, reject) => {
-      uploadTask.on(
-        'state_changed',
-        (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log(`Upload is ${progress}% done`);
-        },
-        (error) => {
-          console.log(error);
-          reject(error);
-        },
-        async () => {
-          const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
-          console.log('File available at:', downloadURL);
-          console.log(downloadURL);
-          resolve(downloadURL);
-        }
-      );
-    });
-  });
+export const uploadImageToFirebase = async (uri) => {
+  let blob;
 
   try {
-    const uploadUrls = await Promise.all(uploadPromises);
-    console.log('All images uploaded');
-    console.log('Image URLs:', uploadUrls);
-    return uploadUrls;
+    const response = await fetch(uri);
+    blob = await response.blob();
   } catch (error) {
-    console.log('Failed to upload images:', error);
-    return [];
+    console.error('There was an error creating the blob:', error);
+    return;
   }
-};
+
+  const storage = getStorage();
+  const imageRef = ref(storage, 'images/' + uuidv4());
+
+  const uploadTask = uploadBytesResumable(imageRef, blob);
+
+  return new Promise((resolve, reject) => {
+    uploadTask.on('state_changed',
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+      },
+      (error) => {
+        console.error('There was an error uploading a file to Cloud Storage:', error);
+        reject(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log('File available at', downloadURL);
+          resolve(downloadURL);
+        });
+      }
+    );
+  });
+}
+
 
 
