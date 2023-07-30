67f9190df200f4a42cdf4e3a247f575f7b06b2db
nweingart <76226527+nweingart@users.noreply.github.com>
2023-07-30 17:56:08 -0400

    add new screens

--- vasco/App.js
+++ vasco/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import 'react-native-get-random-values';
+
 
 // screen imports
-import Register from './screens/Register'
-import Login from './screens/Login'
+import Register from './screens/auth/Register'
+import Login from './screens/auth/Login'
 import Home from './screens/Home'
-import NewDelivery from './screens/NewDelivery/NewDelivery'
-import ReceiptHistory from './screens/ReceiptHistory'
+import NewDelivery from './screens/new/NewDelivery'
+import DeliveryHistory from './screens/feed/DeliveryHistory'
 import SingleReceipt from './screens/SingleReceipt'
 import UploadReceipts from './screens/UploadReceipts'
 import UploadPhotos from './screens/UploadPhotos'
 import Settings from './screens/Settings'
+import Filter from './screens/feed/Filter'
 
 // navigation imports
 import { NavigationContainer } from '@react-navigation/native'
@@ -30,7 +33,8 @@ const App = () => {
           <Stack.Screen options={{ headerShown: false}} name="Login" component={Login} />
           <Stack.Screen options={{ headerShown: false}} name="Home" component={Home} />
           <Stack.Screen options={{ headerShown: false}} name={"NewDelivery"} component={NewDelivery}  />
-          <Stack.Screen options={{ headerShown: false}} name="ReceiptHistory" component={ReceiptHistory} />
+          <Stack.Screen options={{ headerShown: false}} name="DeliveryHistory" component={DeliveryHistory} />
+          <Stack.Screen options={{ headerShown: false}} name="Filter" component={Filter} />
           <Stack.Screen options={{ headerShown: false}} name="SingleReceipt" component={SingleReceipt} />
           <Stack.Screen options={{ headerShown: false}} name={'UploadReceipts'} component={UploadReceipts} />
           <Stack.Screen options={{ headerShown: false}} name={'UploadPhotos'} component={UploadPhotos} />
